3d29dfd0d77ffb0eb5a4d9de7aae1af044931ce6
Mauricio Gracia <mgracia@visionweb.com>
2021-04-19 12:28:56 -0500

    process a xml file line by line and detect declarations - WIP

--- server/src/reference-manager.ts
+++ server/src/reference-manager.ts
@@ -2,11 +2,39 @@ import { ProjectReference } from './project-reference';
 import { ReferenceType } from "./reference-type";
 import { TextDocument } from 'vscode-languageserver-textdocument';
 import { Location } from 'vscode-languageserver';
+import fs = require('fs');
+import readline = require('readline');
+import { sep } from 'path';
+import { fileURLToPath, URL } from 'url';
 
 export class ReferenceManager {
 	refs: ProjectReference[] = [];
 	
-	public update(txtDoc: TextDocument) {
+	public async update(txtDoc: TextDocument) {
+
+		const url = new URL(txtDoc.uri);
+
+		const filePath = fileURLToPath(url);
+
+		const fileStream = fs.createReadStream(filePath);
+
+		const rl = readline.createInterface({
+			input: fileStream,
+			crlfDelay: Infinity
+		});
+		
+		// Note: we use the crlfDelay option to recognize all instances of CR LF
+		// ('\r\n') in input.txt as a single line break.
+	
+		rl.on('line', (line) => {
+			if (line.includes("<function")
+				|| line.includes("<rule")) {
+					console.log(`declaration: ${line}`);
+				}
+			
+		});
+
+		/*************  testing below */
 		let pr: ProjectReference = new ProjectReference("ruleABC", ReferenceType.Rule, txtDoc.uri, 2, true);
 		this.refs.push(pr);
 
